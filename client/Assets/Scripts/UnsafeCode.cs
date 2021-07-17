62e45c0f3e42cf9ec123bea9f08dab317734ece4
Vasilis <35144611+AkyrosXD@users.noreply.github.com>
2021-07-17 18:50:42 +0300

    SubArray function optimized
    Also SubArray got moved to the UnsafeCode class

--- client/Assets/Scripts/UnsafeCode.cs
+++ client/Assets/Scripts/UnsafeCode.cs
@@ -5,11 +5,35 @@ using System.Runtime.InteropServices;
 public static unsafe class UnsafeCode
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe static T ByteArrayToStructure<T>(byte[] data)
+    public static T ByteArrayToStructure<T>(byte[] data)
     {
         fixed (byte* ptr = data)
         {
             return Marshal.PtrToStructure<T>((IntPtr)ptr);
         }
+    }
+
+    // same as memcpy in C
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void* Memcpy(void* dest, void* src, int len)
+    {
+        sbyte* d = (sbyte*)dest;
+        sbyte* s = (sbyte*)src;
+        while (len-- > 0)
+            *d++ = *s++;
+        return dest;
+    }
+
+    // since we cant do ptr + offset in like we can do in C,
+    // we have to come out with another way
+    public static byte[] SubArray(byte[] array, int offset, int length)
+    {
+        byte[] result = new byte[length];
+        fixed (byte* ptr = array, ptrResult = result)
+        {
+            byte* newPtr = ptr + offset;
+            Memcpy(ptrResult, newPtr, length);
+        }
+        return result;
     }
 }
